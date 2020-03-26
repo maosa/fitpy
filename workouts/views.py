b275b36f3a77d6abe8f6a68bce6bd334abb45cd7
maosa <andreasmaos@gmail.com>
2020-03-26 23:09:24 +0200

    added a table of workout logs

--- workouts/views.py
+++ workouts/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+import pandas as pd
 
 # Home page
 
@@ -22,8 +8,24 @@ posts = [
 
 def home(request):
 
+    # Read workout data
+
+    data = pd.read_csv('/Users/maosa/fitpy/data.csv', sep=',')
+
+    # Convert the data frame to a list of dictionaries where
+    # each dictionary in the list is a data frame row
+    # Note: This is only used if one wants to loop over the data frame's rows
+    # If the code on the next line is used, then use {{workouts | safe}} in home.html
+    # to display to table
+
+    # workouts = data.to_dict(orient='records')
+
+    # Alternatively, just convert the data frame to HTML
+
+    workouts = data.to_html(classes='table table-dark')
+
     context = {
-        'posts' : posts
+        'workouts' : workouts
     }
 
     return render(request, 'workouts/home.html', context)
